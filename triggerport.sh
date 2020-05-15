97ac1c0bfa50980eb1a172bb88ede159cd44aed6
Adam Waite <nodice73@gmail.com>
2020-05-14 20:50:04 -0700

    made trigger port a variable and added comments.

--- triggerport.sh
+++ triggerport.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# When run, triggerport periodically sends a packet
+# through the specified port in order to open an incoming port.
+# Necessary if using port triggering to open up your jack ports.
+
+# Set this port as your trigger in your router.
+# The number isn't important, but this one works and won't
+# interfere with other stuff.
+TRIGGERPORT=4031
+
 while true; do
-    echo -n "hello" | nc -4u -w0 google.com 4000
+    echo -n "hello" | nc -4u -w0 google.com $TRIGGERPORT
     sleep 10
 done
